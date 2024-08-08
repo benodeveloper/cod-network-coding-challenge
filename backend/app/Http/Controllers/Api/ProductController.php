f859131b1fcbab74092450c92695f2a441c43ff5
benodeveloper <listoun.developer@gmail.com>
2024-08-08 09:40:50 +0100

    [backend] simple product pagination

--- backend/app/Http/Controllers/Api/ProductController.php
+++ backend/app/Http/Controllers/Api/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Services\ProductService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -14,6 +15,21 @@ class ProductController extends Controller
     public function __construct(ProductService $service)
     {
         $this->service = $service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        $products = $this->service->listProducts(
+            $request->input('per_page', 15)
+        );
+
+        return response()->json($products);
     }
 
     /**
